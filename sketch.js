54f0b4de1477b2e1c1e80a8b7f30cee44a1d1e5e
abhinavprakalp <71956088+abhinavprakalp@users.noreply.github.com>
2020-11-04 10:20:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,8 +39,12 @@ function setup() {
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
 	World.add(world, ground);
+	
+	box1 = createSprite(width/2, 650, 200,20);
+	World.add(world, box1);
+	box1.shapeColor=("red");
 
-	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+	boxBottomBody = Bodies.rectangle(box1Position, box1Y, 200,20 , {isStatic:true} );
 	World.add(world, boxBottomBody);
 
 	box2 = createSprite(495, 590, 10,100);
